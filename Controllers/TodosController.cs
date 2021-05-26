efd55d0ec68b28de978d7baeb70324741ba64227
Anuruddha <contactanuruddha@gamil.com>
2021-05-26 18:33:28 +0530

    2 nd commit

---
+++ Controllers/TodosController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskAPI.Models;
+
+namespace TaskAPI.Controllers
+{
+    //  First Test APP
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Todos() 
+        {
+            var result = GetAllTodos();
+            return Ok(result);
+        }
+
+        private List<Todo> GetAllTodos()
+        {
+            var todos = new List<Todo>();
+
+            var todo = new Todo()
+            {
+                Id = 1,
+                Titel = "School",
+                Description = "School trninng",
+                createDate = DateTime.Now,
+                DueDate = DateTime.Now.AddDays(5),
+                Status = TodoStatus.Inprogess
+            };
+
+            todos.Add(todo);
+
+            var todo1 = new Todo()
+            {
+                Id = 1,
+                Titel = "School",
+                Description = "School trninng",
+                createDate = DateTime.Now,
+                DueDate = DateTime.Now.AddDays(5),
+                Status = TodoStatus.Completed
+            };
+
+            todos.Add(todo1);
+
+
+            var todo2 = new Todo()
+            {
+                Id = 1,
+                Titel = "School",
+                Description = "School trninng",
+                createDate = DateTime.Now,
+                DueDate = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+            };
+
+            todos.Add(todo2);
+
+            return todos;
+        }
+    }
+}
